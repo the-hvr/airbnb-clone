350a05cede9bb0c497b2330afa16794ee5d30a6d
the.hvr <thehvr1@gmail.com>
2026-02-03 21:29:36 +0530

    fix session cookies

--- server.js
+++ server.js
@@ -69,9 +69,11 @@ const sessionOptions = {
     resave: false,
     saveUninitialized : true,
     cookie : {
-        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
         httpOnly : true,
+        secure : true,
+        sameSite : 'lax',
+        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge : 7 * 24 * 60 * 60 * 1000,  
     },  
 }
 app.use(session(sessionOptions));
